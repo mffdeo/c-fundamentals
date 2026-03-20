fda4ebb83920bc9af9133a7b93e018fb69a49c17
Marcos Oliveira <mffdeo@gmail.com>
2026-03-19 21:07:08 -0300

    feat: atualiza o main do day01 com mais 2 tarefas

--- day01/main.c
+++ day01/main.c
@@ -8,18 +8,26 @@ typedef struct {
 
 
 int main() {
-    Tarefa *tarefa = (Tarefa *) malloc(sizeof(Tarefa));
+    // Alocar 3 tarefas
+    Tarefa *tarefa = (Tarefa *) malloc(3*sizeof(Tarefa));
 
     if (tarefa == NULL) {
         printf("Erro de alocacao\n");
         return 1;
     }
 
-    tarefa->id = 1;
-    sprintf(tarefa->descricao, "Estudar C novamente");
+    tarefa[0].id =1;
+    tarefa[1].id =2;
 
-    printf("ID: %d\n", tarefa->id);
-    printf("Descricao: %s\n", tarefa->descricao);
+    sprintf(tarefa[0].descricao, "Teste de tafera 1");
+    sprintf(tarefa[1].descricao, "Teste de tafera 2");
+    
+
+    printf("ID-1: %d\n", tarefa[0].id);
+    printf("Descricao-1: %s\n", tarefa[0].descricao);
+
+    printf("ID-2: %d\n", tarefa[1].id);
+    printf("Descricao-2: %s\n", tarefa[1].descricao);
 
     free(tarefa);
 
