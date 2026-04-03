af770ae7a3f985ab9208a302726ee0d827951bde
Marcos Oliveira <mffdeo@gmail.com>
2026-04-02 22:37:52 -0300

    feat: adiciona nivel de dificuldade

--- projetos/adivinhacao/adivinhacao.c
+++ projetos/adivinhacao/adivinhacao.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-//#define NUMERO_DE_TENTATIVAS 3
 int main(){
 
-    printf("************************************\n");
-    printf("* Bem-vindo ao Jogo de Adivinhação *\n");
-    printf("************************************\n");
+    printf("\n\n");
+    printf("          P  /_\\  P                              \n");
+    printf("         /_\\_|_|_/_\\                            \n");
+    printf("     n_n | ||. .|| | n_n         Bem vindo ao     \n");
+    printf("     |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação! \n");
+    printf("    |\" \"  |  |_|  |\"  \" |                     \n");
+    printf("    |_____| ' _ ' |_____|                         \n");
+    printf("          \\__|_|__/                              \n");
+    printf("\n\n");
 
     int numeroSecreto;
     // Gera um número aleatório ate 100;
@@ -15,36 +20,76 @@ int main(){
     numeroSecreto = rand() % 100;
     int chute;
     int ganhou = 0;
-    int tentativas = 1;
+
     int pontos = 1000;
-    
-    while (!ganhou)
-    {
+    int nivel;
+    int totaldetentativas;
+    int acertou=0;
+    printf("Qual o nivel de dificuldade?\n");
+    printf("(1) Facil (2) Medio (3) Dificil\n");
+
+
+    scanf("%d", &nivel);
+    if(nivel == 1) {
+        totaldetentativas = 20;
+    } else if (nivel == 2) {
+        totaldetentativas = 15;
+    } else {
+        totaldetentativas = 6;
+    }
+
+    for(int i = 1; i <= totaldetentativas; i++) {
+        printf("Tentativa %d de %d\n", i, totaldetentativas);
         printf("Qual o seu chute?\n");
         scanf("%d", &chute);
-
         if(chute < 0) {
             printf("Você não pode chutar números negativos\n");
+            i--;
             continue;
         }
-
-        printf("Seu %do. chute foi %d\n", tentativas, chute);
-        int acertou = chute == numeroSecreto;
+    // ...
+        acertou = chute == numeroSecreto;
         int maior = chute > numeroSecreto;
         if(acertou) {
-            printf("Parabéns! Você acertou!\n");
-            ganhou = 1;
-        } else if(maior) {
+            break;
+        }else if(maior) {
         printf("Seu chute foi maior do que o número secreto!\n");
         } else {
         printf("Seu chute foi menor do que o número secreto!\n");
         }
-        tentativas++;
         pontos = pontos - (abs(chute - numeroSecreto))/2;
     }
-    
-    printf("\nObrigado por jogar!\n");
-    printf("Você fez %d pontos\n", pontos);
-    printf("Fim de jogo\n");
+
+    printf("\n");
+    if(acertou) {
+        printf("             OOOOOOOOOOO               \n");
+        printf("         OOOOOOOOOOOOOOOOOOO           \n");
+        printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+        printf("    OOOOOO      OOOOO      OOOOOO      \n");
+        printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+        printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+        printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+        printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+        printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+        printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+        printf("         OOOOOO         OOOOOO         \n");
+        printf("             OOOOOOOOOOOO              \n");
+        printf("\nParabéns! Você acertou!\n");
+        printf("Você fez %d pontos. Até a próxima!\n\n", pontos);
+    } else {
+ 
+        printf("       \\|/ ____ \\|/    \n");   
+        printf("        @~/ ,. \\~@      \n");   
+        printf("       /_( \\__/ )_\\    \n");   
+        printf("          \\__U_/        \n");
+ 
+        printf("\nVocê perdeu! Tente novamente!\n\n");
+    }
     return 0;
 }
+
+
+ 
